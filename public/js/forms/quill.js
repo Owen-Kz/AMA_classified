2146cdace3fc265511e1b38ec6342a013fb3a3fc
Benson Michael Owen <lordkz19@gmail.com>
2024-08-30 04:55:54 +0100

    CDN Delevery for Quill

---
+++ public/js/forms/quill.js
@@ -0,0 +1,14 @@
+const quill = new Quill("#editor", {
+  modules: {
+    toolbar: [
+      ["bold", "italic", "underline"],
+      ["link", "blockquote", "code-block", "image"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ header: [1, 2, false] }],
+      [{ align: [] }],
+     
+    ],
+    
+  },
+  theme: "snow",
+});
