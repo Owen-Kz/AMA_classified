328a932a03b3d25f71c84e57ae92a69bc3923119
Benson Michael Owen <lordkz19@gmail.com>
2024-08-30 09:31:56 +0100

    Memebers page updated for admin

--- controllers/admin/members.js
+++ controllers/admin/members.js
@@ -2,7 +2,8 @@ const Members = async (req,res) =>{
     if(req.cookies._ama){
     res.render("membersList", {username:req.user.u_name})
     }else{
-        return req.json({error:"NotLoggedIn"})
+        res.render("adminLogin")
+        // return res.json({error:"NotLoggedIn"})
     }
 }
 
