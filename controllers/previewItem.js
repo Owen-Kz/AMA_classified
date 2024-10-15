06453ae430f90dcbceef2884a4450894e24627f0
Benson Michael Owen <lordkz19@gmail.com>
2024-10-15 22:50:58 +0100

    Google ads Scirpt added

--- controllers/previewItem.js
+++ controllers/previewItem.js
@@ -1,6 +1,6 @@
 const previewItem =  async (req,res) =>{
    
-    res.render("preview.ejs", {title:req.params.productTitle,email:req.user.email, username:req.user.u_name, firstname:req.user.name, lastname:req.user.l_name, country:req.user.country, phonenumber: req.user.phone, profilePhoto:req.user.pp, user_id:req.user.id})
+    res.render("preview.ejs", {title:req.params.productTitle, email:req.user.email, username:req.user.u_name, firstname:req.user.name, lastname:req.user.l_name, country:req.user.country, phonenumber: req.user.phone, profilePhoto:req.user.pp, user_id:req.user.id})
   
 }
 
